187030fd291d69bb88ecac8d86543e4c4d4897ce
Lachlan Deer <lachlan.deer@gmail.com>
2022-09-15 15:30:18 +0200

    oj script update

--- data/oj_data.R
+++ data/oj_data.R
@@ -4,3 +4,8 @@ url <- "https://raw.githubusercontent.com/TaddyLab/MBAcourse/master/examples/oj.
 demand <- read_csv(url)
 
 write_csv(demand, "oj.csv")
+
+summary(lm(price ~ feat, data = demand))
+
+demand$price
+lm(price ~ feat, df = demand)
